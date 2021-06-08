97adb3fcb84fd4e5c19bda8558fb5c61c3110ab5
mosesr-kim <mosesrandykim@gmail.com>
2021-06-08 20:00:48 +0000

    created all elements needed for the header.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var xhr = new XMLHttpRequest();
+// xhr.addEventListener('load', function () {
+//   console.log(this.status);
+//   console.log(this.response);
+// });
+// xhr.open("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-movers?region=US&lang=en-US&count=6&start=0");
+// xhr.responseType = 'json';
+// xhr.setRequestHeader("x-rapidapi-key", "869820100bmsh7cc30b317c45153p1f792fjsn21c1cc0536d7");
+// xhr.setRequestHeader("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com");
+// xhr.send();
