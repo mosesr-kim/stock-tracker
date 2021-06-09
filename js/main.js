e9f4d26eb9baf0f6c74f189f17916e46bda5030c
Moses Kim <mosesrandykim@gmail.com>
2021-06-09 00:31:12 -0700

    created an entry via DOM that included the stock's full company name, ticker symbol, current market price, today's percentage, today's low, today's high, and the description of the company. in that entry, also added a read more button and an icon.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
 var $search = document.querySelector('.searchForm');
+var $main = document.querySelector('main');
 
 $search.addEventListener('submit', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
-  $search.reset();
+  console.log($search.elements.search.value);
+  if ($search.elements.search.value.toUpperCase() === 'GME') {
+    console.log(gmeSearch);
+    $search.reset();
+    createStockEntry(gmeSearch);
+  } else if ($search.elements.search.value.toUpperCase() === 'AMC') {
+    console.log(amcSearch);
+    $search.reset();
+    return amcSearch;
+  } else if ($search.elements.search.value.toUpperCase() === 'BB') {
+    console.log(bbSearch);
+    $search.reset();
+    return bbSearch;
+  } else if ($search.elements.search.value.toUpperCase() === 'NOK') {
+    console.log(nokSearch);
+    $search.reset();
+    return nokSearch;
+  } else if ($search.elements.search.value.toUpperCase() === 'TSLA') {
+    console.log(tslaSearch);
+    $search.reset();
+    return tslaSearch;
+  } else if ($search.elements.search.value.toUpperCase() === 'GOOGL') {
+    console.log(googlSearch);
+    $search.reset();
+    return googlSearch;
+  } else {
+    $search.reset();
+    return 'Please enter a valid stock ticker symbol';
+  }
+  // $search.reset();
   // (Code below is commented out to avoid rate limiting restrictions)
   // Gets the stock ticker symbol from the form and runs the searchRequest function which requests data from the api.
   // data.search = $search.elements.search.value;
@@ -42,3 +72,74 @@ function handleSearch(event) {
 //   xhr.setRequestHeader('x-rapidapi-host', 'apidojo-yahoo-finance-v1.p.rapidapi.com');
 //   xhr.send();
 // }
+
+// container
+//   row
+//     h1 h1 h1
+//   row
+//     h2 h2 h2
+//   p
+//   row
+//     button icon
+
+function createStockEntry(data) {
+  var stockContainer = document.createElement('div');
+  stockContainer.className = 'container';
+  $main.appendChild(stockContainer);
+
+  var headerRow = document.createElement('div');
+  headerRow.className = 'row';
+  stockContainer.appendChild(headerRow);
+
+  var stockName = document.createElement('h1');
+  stockName.className = 'stockName';
+  stockName.textContent = data.price.longName + ':';
+  headerRow.appendChild(stockName);
+
+  var stockSymbol = document.createElement('h1');
+  stockSymbol.className = 'stockSymbol';
+  stockSymbol.textContent = data.price.symbol;
+  headerRow.appendChild(stockSymbol);
+
+  var stockPrice = document.createElement('h1');
+  stockPrice.className = 'stockPrice';
+  stockPrice.textContent = '$' + data.price.regularMarketPrice.fmt;
+  headerRow.appendChild(stockPrice);
+
+  var subHeaderRow = document.createElement('div');
+  subHeaderRow.className = 'row';
+  stockContainer.appendChild(subHeaderRow);
+
+  var todayPercentage = document.createElement('h2');
+  todayPercentage.className = 'stockToday';
+  todayPercentage.textContent = 'Today: ' + data.price.regularMarketChangePercent.fmt;
+  subHeaderRow.appendChild(todayPercentage);
+
+  var todayLow = document.createElement('h2');
+  todayLow.className = 'stockLow';
+  todayLow.textContent = 'Low: $' + data.price.regularMarketDayLow.fmt;
+  subHeaderRow.appendChild(todayLow);
+
+  var todayHigh = document.createElement('h2');
+  todayHigh.className = 'stockHigh';
+  todayHigh.textContent = 'High: $' + data.price.regularMarketDayHigh.fmt;
+  subHeaderRow.appendChild(todayHigh);
+
+  var companySummary = document.createElement('p');
+  companySummary.className = 'companySummary';
+  companySummary.textContent = data.assetProfile.longBusinessSummary;
+  stockContainer.appendChild(companySummary);
+
+  var buttonRow = document.createElement('div');
+  buttonRow.className = 'buttonRow';
+  stockContainer.appendChild(buttonRow);
+
+  var readMoreButton = document.createElement('button');
+  readMoreButton.className = 'readMore';
+  readMoreButton.textContent = 'Read More';
+  buttonRow.appendChild(readMoreButton);
+
+  var addStock = document.createElement('i');
+  addStock.className = 'fas fa-plus-circle';
+  buttonRow.appendChild(addStock);
+}
