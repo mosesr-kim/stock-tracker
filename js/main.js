5f518bfd08b6716d89e7b313a2d4b1baf5f59d3f
Moses Kim <mosesrandykim@gmail.com>
2021-06-17 12:59:37 -0700

    added a loading message that only shows when the user searches for a stock. the message disappears once the data from the api is retrieved and displayed as a search result.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ var $trendingContainer = document.querySelector('.trendingContainer');
 var $trendingStockEntries = document.querySelector('.trendingStockEntries');
 var $views = document.querySelectorAll('.view');
 var $logo = document.querySelector('.logo');
+var $loading = document.querySelector('.loading');
 
 $search.addEventListener('submit', handleSearch);
 $main.addEventListener('click', handleAddStock);
@@ -32,46 +33,51 @@ function handleSearch(event) {
   event.preventDefault();
   removeSearchEntry();
   viewSwap('search');
-  data.editing = false;
-  $searchResultHeader.className = 'row searchResultHeader';
+  $searchResultHeader.className = 'hidden';
+  $loading.className = 'row loading';
   $editHeader.className = 'hidden';
+  data.editing = false;
+  // $searchResultHeader.className = 'row searchResultHeader';
+  // $editHeader.className = 'hidden';
   data.search = $search.elements.search.value;
-  // searchRequest(data.search);
-  for (var key in requests) {
-    if (data.search.toUpperCase() === key) {
-      data.searchResult = requests[key];
-      var stockSearchDOM = createStockEntry(requests[key]);
-      $searchContainer.appendChild(stockSearchDOM);
-      var deleteButton = document.querySelector('.fa-minus-circle');
-      deleteButton.className = 'hidden';
-    }
-    $search.reset();
-  }
+  searchRequest(data.search);
+  // for (var key in requests) {
+  //   if (data.search.toUpperCase() === key) {
+  //     data.searchResult = requests[key];
+  //     var stockSearchDOM = createStockEntry(requests[key]);
+  //     $searchContainer.appendChild(stockSearchDOM);
+  //     var deleteButton = document.querySelector('.fa-minus-circle');
+  //     deleteButton.className = 'hidden';
+  //   }
+  //   $search.reset();
+  // }
   // (Code below is commented out to avoid rate limiting restrictions)
   // Gets the stock ticker symbol from the form and runs the searchRequest function which requests data from the api.
 }
 
 // (Code below is commented out to avoid rate limiting restrictions)
 // The function to request Stock/V2/Get-Profile of a certain stock ticker symbol.
-// function searchRequest(search) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.addEventListener('load', function () {
-//     console.log(this.status);
-//     console.log(this.response);
-//     data.searchResult = this.response;
-//     var stockSearchDOM = createStockEntry(data.searchResult);
-//     $searchContainer.appendChild(stockSearchDOM);
-//     var deleteButton = document.querySelector('.fa-minus-circle');
-//     deleteButton.className = 'hidden';
-//     $search.reset();
-//     return data.searchResult;
-//   });
-//   xhr.open('GET', 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile?symbol=' + search + '&region=US');
-//   xhr.responseType = 'json';
-//   xhr.setRequestHeader('x-rapidapi-key', '869820100bmsh7cc30b317c45153p1f792fjsn21c1cc0536d7');
-//   xhr.setRequestHeader('x-rapidapi-host', 'apidojo-yahoo-finance-v1.p.rapidapi.com');
-//   xhr.send();
-// }
+function searchRequest(search) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function () {
+    console.log(this.status);
+    console.log(this.response);
+    data.searchResult = this.response;
+    var stockSearchDOM = createStockEntry(data.searchResult);
+    $searchContainer.appendChild(stockSearchDOM);
+    var deleteButton = document.querySelector('.fa-minus-circle');
+    deleteButton.className = 'hidden';
+    $searchResultHeader.className = 'row searchResultHeader';
+    $loading.className = 'hidden';
+    $search.reset();
+    return data.searchResult;
+  });
+  xhr.open('GET', 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile?symbol=' + search + '&region=US');
+  xhr.responseType = 'json';
+  xhr.setRequestHeader('x-rapidapi-key', '869820100bmsh7cc30b317c45153p1f792fjsn21c1cc0536d7');
+  xhr.setRequestHeader('x-rapidapi-host', 'apidojo-yahoo-finance-v1.p.rapidapi.com');
+  xhr.send();
+}
 
 // function trendingSearchRequest(search) {
 //   var xhr = new XMLHttpRequest();
